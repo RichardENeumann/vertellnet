f6e654ce3bcf6753a0bf25033b17815774d06d74
RichardENeumann <RichardNeumann@googlemail.com>
2023-10-19 17:31:27 +0200

    Add URL Rewrite for API

--- wbget/index.php
+++ wbget/index.php
@@ -1,13 +1,13 @@
 <?php declare(strict_types=1);
 
-require("inc/wbAPI.php");
+require("../wb/inc/wbAPI.php");
 
 if (!isset($_GET["search"])) {
-    echo "Error: No search string given.\n";
+    echo "Error: No search string given. (search = empty)\n";
 } else if (!isset($_GET["lang"]) || (!preg_match("/^plat$|^hoog$/i", $_GET["lang"]))) {
-    echo "Error: Language preference not stated correctly. (hoog|plat)\n";
+    echo "Error: Language preference not stated correctly. (lang = hoog|plat)\n";
 } else if (!isset($_GET["return"]) || (!preg_match("/^html$|^xml$|^json$/i", $_GET["return"]))) {
-    echo "Error: Result return type not stated correctly. (html|xml|json)\n";
+    echo "Error: Result return type not stated correctly. (return = html|xml|json)\n";
 } else {
     wbParseRequest($_GET["search"], $_GET["lang"], $_GET["return"]);    
 }
